98a37bff82d1818d542b876c55228f013ef3826e
Hamza Khan <khan_hamza95@hotmail.com>
2024-03-21 23:12:00 -0400

    first commit

---
+++ src/main/java/com/hamzaKhan/fullStackbackEnd/FullStackBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.hamzaKhan.fullStackbackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamzaKhan/fullStackbackEnd/FullStackBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamzaKhan.fullStackbackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
